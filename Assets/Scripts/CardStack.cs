1cf09bbf46dcc89bae1a8e5662e4943384057d86
Adam Kay <kayad01@gettysburg.edu>
2015-05-24 18:00:57 -0600

    Added new script, CardStack

---
+++ Assets/Scripts/CardStack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Used for any stack of cards. Decks, discard piles, etc.
+/// </summary>
+public class CardStack : Extender {
+
+	/// <summary>
+	/// The list of cards in this stack.
+	/// </summary>
+	public List<Card> cardList;
+
+	/// <summary>
+	/// Index of the card on top of the deck.
+	/// </summary>
+	public int index;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Shuffle() {
+		for (int i = 0; i < cardList.Count; i++) {
+			Card temp = cardList[i];
+			int randomIndex = Random.Range(i, cardList.Count);
+			cardList[i] = cardList[randomIndex];
+			cardList[randomIndex] = temp;
+		}
+	}
+}
