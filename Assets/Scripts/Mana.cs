75f18faac995d4de5af28928974500c208a3d6d3
Adam Kay <kayad01@gettysburg.edu>
2015-05-29 03:10:32 -0600

    Added Mana script and Mana Display to UI

---
+++ Assets/Scripts/Mana.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mana : Extender {
+
+    public ManaInfo manaState;
+    public bool manaStateChanged = false;
+
+    public void AddMana(int howMuch)
+    {
+        manaState.CurrentMana += howMuch;
+        manaStateChanged = true;
+    }
+
+    public void SetMana(int number)
+    {
+        manaState.CurrentMana = number;
+        manaStateChanged = true;
+    }
+
+    public void SubtractMana(int howMuch)
+    {
+        manaState.CurrentMana -= howMuch;
+        manaStateChanged = true;
+    }
+ 
+}
+
+[System.Serializable]
+public class ManaInfo
+{
+    public int currentMana;
+    public int maxMana;
+
+    public int CurrentMana
+    {
+        get
+        {
+            return currentMana;
+        }
+        set
+        {
+            currentMana = value;
+            if (currentMana > maxMana)
+                currentMana = maxMana;
+        }
+    }
+
+    public int MaxMana
+    {
+        get
+        {
+            return maxMana;
+        }
+        set
+        {
+            maxMana = value;
+            if (currentMana > maxMana)
+                currentMana = maxMana;
+        }
+    }
+}
---
+++ Assets/Scripts/ManaDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ManaDisplay : MonoBehaviour {
+
+    Mana mana;
+    Text textDisplay;
+
+	// Use this for initialization
+	void Start () {
+        mana = GameObject.FindGameObjectWithTag("World").GetSafeComponent<Mana>();
+        textDisplay = this.gameObject.GetSafeComponent<Text>();
+        textDisplay.text = mana.manaState.CurrentMana.ToString() + " / " + mana.manaState.MaxMana.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mana.manaStateChanged)
+        {
+            textDisplay.text = mana.manaState.CurrentMana.ToString() + " / " + mana.manaState.MaxMana.ToString();
+            mana.manaStateChanged = false;
+        }
+	}
+}
