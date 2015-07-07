8393e16e415e5a30644215f2c0f88978104fe192
jmbiggs <eraserhead@gmail.com>
2015-07-06 22:18:25 -0600

    Draws random cards from Cards folder now

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -34,35 +34,7 @@ public class Hand : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Z))
         {
-
-            // not the best way to draw a card, but obviously we'll change this later
-
-            GameObject newCard = Instantiate(Resources.Load("Card Canvas")) as GameObject;
-
-            // NOT WORKING YET
-            /*
-            GameObject[] tmpcards = Resources.LoadAll<GameObject>("Cards");
-
-            newCard.GetComponentInChildren<Card> = Instantiate(tmpcards[Random.Range(0, tmpcards.Length)]) as GameObject;
-            
-             */ 
-             
-             
-            newCard.transform.position = spawnPoint.position;
-
-             
-            // giving it an id number for easier identification during testing            
-            newCard.name = "Card Canvas: " + drawnCardsIndex.ToString();
-            
-            // set up correct layer order (to properly allow for mouse over)
-            Canvas ncCanv = newCard.GetComponent<Canvas>();
-            ncCanv.sortingOrder = drawnCardsIndex;
-
-            // increment index
-            drawnCardsIndex++;
-
-            newCard.GetComponentInChildren<Image>().color = new Color(Random.value, Random.value, Random.value);
-            cards.Add(newCard);
+            DrawCard();
 
             foreach (GameObject go in cards)
             {
@@ -71,8 +43,51 @@ public class Hand : MonoBehaviour {
         }
 	}
 
+    public void DrawCard()
+    {
+        GameObject newCard = Instantiate(Resources.Load("Card Canvas")) as GameObject;
+
+        // not the best way to draw a card, but obviously we'll change this later
+
+        GameObject[] tmpcards = Resources.LoadAll<GameObject>("Cards");
+        GameObject drawnCard = tmpcards[Random.Range(0, tmpcards.Length)];
+
+        // copy info from drawncard to newcard
+        Card drawnCardInfo = drawnCard.GetComponent<Card>();
+        Card newCardInfo = newCard.GetComponentInChildren<Card>();
+
+        newCardInfo.cardID = drawnCardInfo.cardID;
+        newCardInfo.characterRestriction = drawnCardInfo.characterRestriction;
+        newCardInfo.jobRestriction = drawnCardInfo.jobRestriction;
+        newCardInfo.cardName = drawnCardInfo.cardName;
+        newCardInfo.manaCost = drawnCardInfo.manaCost;
+        newCardInfo.cardText = drawnCardInfo.cardText;
+        newCardInfo.cardKeywords = drawnCardInfo.cardKeywords;
+        newCardInfo.cardActions = drawnCardInfo.cardActions;
+
+        // spawn in position
+
+        newCard.transform.position = spawnPoint.position;
+
+        // giving it an id number for easier identification during testing            
+        newCard.name = "Card Canvas: " + drawnCardsIndex.ToString();
+
+        // set up correct layer order (to properly allow for mouse over)
+        Canvas ncCanv = newCard.GetComponent<Canvas>();
+        ncCanv.sortingOrder = drawnCardsIndex;
+
+        // increment index
+        drawnCardsIndex++;
+
+        newCard.GetComponentInChildren<Image>().color = new Color(Random.value, Random.value, Random.value);
+        cards.Add(newCard);
+
+    }
+
     public void UpdateCardPlacement()
     {
         
     }
+
+
 }
